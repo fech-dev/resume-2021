a7fb087320b5028bb0de7f6cf106a3ef6d15fd57
Federico <federico_mameli@yahoo.com>
2021-08-30 10:17:22 +0200

    feat(components): create ContactsList

---
+++ src/components/ContactsList.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+  name: "ContactsList",
+  functional: true,
+  props: {
+    contacts: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  render(h, { props, data }) {
+    const getLiContent = ({ href, text, icon }) => {
+      const Icon = h("b-icon", {
+        props: { icon },
+      });
+
+      const LinkWrap = h("a", { attrs: { href } }, [Icon, text]);
+
+      return href ? [LinkWrap] : [Icon, text];
+    };
+
+    const ContactsItems = props.contacts.map((contact) =>
+      h(
+        "li",
+        {
+          staticClass: "contacts-list__item",
+        },
+        getLiContent(contact)
+      )
+    );
+
+    return h(
+      "ul",
+      {
+        staticClass:
+          "contacts-list list-unstyled d-flex flex-column flex-md-row flex-md-wrap",
+        class: [data.staticClass, data.class],
+      },
+      ContactsItems
+    );
+  },
+};
+</script>
+
+<style lang="scss">
+.contacts-list {
+  gap: 0.5rem 1rem;
+
+  &__item > a {
+    color: currentColor;
+  }
+
+  &__item,
+  &__item a {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+}
+</style>
