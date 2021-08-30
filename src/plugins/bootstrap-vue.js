5455628d1dbe333c3807761e31ca49d6583727e7
Federico <federico_mameli@yahoo.com>
2021-08-30 10:13:40 +0200

    feat(icons): add bootstrap Icons

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,5 +1,25 @@
 import Vue from "vue";
 
 import BootstrapVue from "bootstrap-vue";
+import {
+  BIcon,
+  BIconEnvelopeFill,
+  BIconLinkedin,
+  BIconMap,
+  BIconPhoneFill,
+  BIconGithub,
+} from "bootstrap-vue";
 
 Vue.use(BootstrapVue);
+
+//icons
+Object.entries({
+  BIcon,
+  BIconEnvelopeFill,
+  BIconLinkedin,
+  BIconMap,
+  BIconPhoneFill,
+  BIconGithub,
+}).forEach(([componentName, component]) => {
+  Vue.component(componentName, component);
+});
