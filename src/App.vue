2bd9e1a792d1455dfe05e7738f148c0b2653e73a
Federico <federico_mameli@yahoo.com>
2021-08-30 10:18:27 +0200

    feat(app). create basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <app-header :contacts="contacts" />
+
+    <main class="main container-xl">
+      <app-skills :skills="skills" />
+      <app-jobs />
+      <app-education />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppHeader from "@/components/AppHeader.vue";
+import AppSkills from "@/components/AppSkills";
+import AppJobs from "@/components/AppJobs";
+import AppEducation from "@/components/AppEducation";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    AppHeader,
+    AppSkills,
+    AppJobs,
+    AppEducation,
+  },
+
+  data() {
+    return {
+      contacts: [
+        {
+          icon: "envelope-fill",
+          text: "federico_mameli@yahoo.it",
+          href: "mailto:federico_mameli@yahoo.it",
+        },
+        {
+          icon: "phone-fill",
+          text: "3488077577",
+          href: "tel:3488077577",
+        },
+        {
+          icon: "map",
+          text: "Cagliari, Sardegna",
+        },
+        {
+          icon: "linkedin",
+          text: "Linkedn",
+          href: "https://www.linkedin.com/in/federicomamelifech/",
+        },
+        {
+          icon: "github",
+          text: "Github",
+          href: "https://github.com/fech-dev",
+        },
+      ],
+      skills: [
+        { name: "HTML", vote: 5 },
+        { name: "CSS", vote: 5 },
+        { name: "Javascript", vote: 4.5 },
+        { name: "Vue.js", vote: 4.5 },
+        { name: "Nuxt.js", vote: 4 },
+        { name: "Node.js", vote: 3 },
+        { name: "MongoDB", vote: 3 },
+        { name: "Webpack", vote: 4 },
+      ],
+    };
   },
 };
 </script>
 
 <style lang="scss">
+@import "~@/assets/scss/vendors/bootstrap-vue/index";
+
+.img-cover {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.bg-gradient-primary {
+  background-image: linear-gradient(
+    175deg,
+    theme-color("primary") 35%,
+    color("teal") 82.5%
+  );
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  .main {
+    /*  display: grid;
+    grid-template-columns: 0.3fr 0.7fr;
+    gap: 1.5rem 0; */
+
+    #skills {
+      /*  grid-row: 1 / -1;
+      grid-column: 1 / span 1; */
+    }
+
+    #jobs,
+    #education {
+      /* grid-column: 2 / span 1; */
+    }
+  }
 }
 </style>
