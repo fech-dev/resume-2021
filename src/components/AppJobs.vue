31f157acc2e0d0445db37467ac834273e778f83e
Federico <federico_mameli@yahoo.com>
2021-09-07 12:23:36 +0200

    feat(app-jobs): add transitions

--- src/components/AppJobs.vue
+++ src/components/AppJobs.vue
@@ -3,42 +3,42 @@
     <h2 class="title">Esperienza Lavorativa</h2>
 
     <div class="px-4 mt-4">
-      <section-content-card
-        title="Front End Web Developer - Smart contact"
-        :date-from="format(new Date('2018-06-01'), 'MMM yyyy')"
-        date-to="attuale"
-      >
-        Frontend developer nella sezione digital.
-        <ul class="list-disc pl-4">
-          <li>
-            Principale sviluppo e mantenimento di landing page per varie aziende
-            operanti nel campo dell'energia, telefonia e internet finalizzate
-            alla lead generation.
-          </li>
-          <li>
-            Sviluppo e manuntenzione di script/packages per il funzionamento del
-            core e features delle landing, rendendo la creazione modulare,
-            semplice e veloce.
-          </li>
-          <li>Organizzazione del team e formazione tirocinanti.</li>
-          <li>
-            Implementazione motore di comparazione all'interno del sito
-            prezzogiusto.com.
-          </li>
-        </ul>
-      </section-content-card>
+      <transition appear enter-active-class="animate__animated animate__fadeIn">
+        <section-content-card
+          title="Front End Web Developer - Smart contact"
+          :date-from="format(new Date('2018-06-01'), 'MMM yyyy')"
+          date-to="attuale"
+        >
+          Frontend developer nella sezione digital.
+          <ul class="list-disc pl-4">
+            <li>
+              Principale sviluppo e mantenimento di landing page per varie
+              aziende operanti nel campo dell'energia, telefonia e internet
+              finalizzate alla lead generation.
+            </li>
+            <li>
+              Sviluppo e manuntenzione di script/packages per il funzionamento
+              del core e features delle landing, rendendo la creazione modulare,
+              semplice e veloce.
+            </li>
+            <li>Organizzazione del team e formazione tirocinanti.</li>
+            <li>
+              Implementazione motore di comparazione all'interno del sito
+              prezzogiusto.com.
+            </li>
+          </ul>
+        </section-content-card>
+      </transition>
     </div>
   </section>
 </template>
 
 <script>
-import { format } from "date-fns";
+import formatDateMixin from "@/mixins/dateFormat";
 
 export default {
   name: "AppJobs",
-  methods: {
-    format,
-  },
+  mixins: [formatDateMixin],
 };
 </script>
 
