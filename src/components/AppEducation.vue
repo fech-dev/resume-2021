b2b2bf3decc8b4db8f7ca396ee49b06d08b5a2e7
Federico <federico_mameli@yahoo.com>
2021-09-09 20:27:39 +0200

    refact(education): change structure & copy

--- src/components/AppEducation.vue
+++ src/components/AppEducation.vue
@@ -21,6 +21,7 @@
 
 <script>
 import formatDateMixin from "@/mixins/dateFormat";
+import marked from "marked";
 
 export default {
   mixins: [formatDateMixin],
@@ -29,33 +30,27 @@ export default {
     return {
       educations: [
         {
-          title: "Corso Algoritmi e Strutture Dati",
+          title: "Corsi Attuali",
           dateFrom: this.format(new Date("2021-01-01"), "yyyy"),
+          body: marked(
+            "Attualmente sto seguendo un corso di [Strutture dati e Algoritmi](https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass) e un corso su [TypeScript](https://www.udemy.com/course/understanding-typescript/)"
+          ),
         },
         {
-          title: "Corso Typescript",
-          dateFrom: this.format(new Date("2021-01-01"), "yyyy"),
-          dateTo: this.format(new Date("2021-01-01"), "yyyy"),
-        },
-        {
-          title: "Corso Vue",
-          dateFrom: this.format(new Date("2018-01-01"), "yyyy"),
-          dateTo: this.format(new Date("2019-01-01"), "yyyy"),
-        },
-        {
-          title: "Corso CSS",
-          dateFrom: this.format(new Date("2018-01-01"), "yyyy"),
-          dateTo: this.format(new Date("2018-01-01"), "yyyy"),
-        },
-        {
-          title: "Corso Javascript",
+          title: "Corsi Seguiti",
           dateFrom: this.format(new Date("2017-01-01"), "yyyy"),
-          dateTo: this.format(new Date("2018-01-01"), "yyyy"),
+          dateTo: this.format(new Date("2020-12-01"), "yyyy"),
+          body: marked(
+            "Dopo il diploma ho seguito vari corsi tra cui: \n- *CSS*: (https://www.udemy.com/course/advanced-css-and-sass/) \n- *JS*: (https://www.udemy.com/course/the-complete-javascript-course)\n- *Vue*: (https://www.udemy.com/course/vuejs-2-the-complete-guide/)\n - *Node.js*: (https://www.udemy.com/course/nodejs-express-mongodb-bootcamp)"
+          ),
         },
         {
           title: "Diploma Informatica e Telecomunicazioni",
           dateFrom: this.format(new Date("2011-09-01"), "MMM yyyy"),
           dateTo: this.format(new Date("2017-07-01"), "MMM yyyy"),
+          body: marked(
+            "Le tematiche affrontate sono: \n- Fondamenti di informatica.\n- Sistemi e Reti.\n- Programmazione a oggetti.\n- Programmazione web.\n\n I linguaggi utilizzati sono: C, Java, HTML, CSS, PHP, MySql."
+          ),
         },
       ],
     };
